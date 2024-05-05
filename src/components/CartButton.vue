44acb724cc3a8a22af59e0eac796b5cdc7172677
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:22:51 +0400

    cartButton to show number of cart items in header

---
+++ src/components/CartButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useCart } from "@/stores/cart";
+import { storeToRefs } from "pinia";
+import { SfBadge, SfButton, SfIconShoppingCart } from "@storefront-ui/vue";
+const { totalItems } = storeToRefs(useCart());
+</script>
+
+<template>
+  <RouterLink to="/shopping-cart">
+    <SfButton
+      class="relative text-white hover:text-white active:text-white hover:bg-primary-800 active:bg-primary-900 mr-2 -ml-0.5 rounded-md"
+      aria-label="Cart"
+      variant="tertiary"
+      square
+    >
+      <SfIconShoppingCart />
+      <SfBadge :content="totalItems" class="bg-white cart-badge" />
+    </SfButton>
+  </RouterLink>
+</template>
