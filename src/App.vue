e71a35eebef5d9d84934949f03c1d8ba3116020b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 13:23:13 +0400

    app.vue

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import Layout from "./components/Layout.vue";
+</script>
+
+<template>
+  <Layout>
+    <RouterView />
+  </Layout>
+
+  <notifications position="bottom right" />
+</template>
