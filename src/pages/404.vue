0a9d82995a6e4f880680ae1a93990a625e0096db
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 13:23:28 +0400

    created 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { SfButton } from "@storefront-ui/vue";
+</script>
+
+<template>
+  <div class="flex justify-center my-10">
+    <div class="w-100 text-secondary text-center">
+      <h2 class="mb-3">Page Not Found 🕵🏻‍♀️</h2>
+      <RouterLink to="/">
+        <SfButton> Home </SfButton>
+      </RouterLink>
+      <img src="/404.svg" alt="Error page" class="img-fluid" />
+    </div>
+  </div>
+</template>
