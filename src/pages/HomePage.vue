e54953a27b75a9ea72700d79b89c45d6ad0d978e
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 13:57:33 +0400

    created home page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Categories from "@/components/Categories.vue";
+import ProductList from "@/components/ProductList.vue";
+import Card from "@/components/Card.vue";
+</script>
+
+<template>
+  <div class="mb-10 px-4 md:px-0"></div>
+
+  <div class="md:flex md:justify-start gap-6">
+    <Card class="md:max-w-[300px] w-full mb-auto">
+      <Categories />
+    </Card>
+
+    <div class="flex-1">
+      <ProductList />
+    </div>
+  </div>
+</template>
