f8d50ce091a086c2118b699910210cf379a7fac3
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:27:22 +0400

    created a store to hold categories

---
+++ src/stores/main.ts
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+import { ref } from 'vue';
+import { type Categories } from "@/types";
+
+export const useMain = defineStore("main", () => {
+    const categories = ref<Categories[]>([]);
+
+    const setCategories = (data: Categories[]) => {
+        categories.value = data;
+    };
+    return {
+        setCategories,
+        categories,
+    };
+});
