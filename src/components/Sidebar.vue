b9758ae9c936a6fa077e7b80a36cd933bc1e490f
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:18:22 +0400

    Sidebar component for categories and possibley other filters

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import Card from "@/components/Card.vue";
+import CategoriesList from "@/components/CategoriesList.vue";
+</script>
+
+<template>
+  <Card class="md:max-w-[300px] w-full mb-auto">
+    <div class="w-full hidden md:block">
+      <h2
+        class="rounded-md px-6 my-4 text-lg font-semibold block text-primary-700"
+      >
+        Categories
+      </h2>
+      <CategoriesList />
+    </div>
+  </Card>
+</template>
