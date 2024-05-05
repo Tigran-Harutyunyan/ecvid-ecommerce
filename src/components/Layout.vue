1b33baf872c78a16c3842b698dc1585835451835
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:16:57 +0400

    created Layout component

---
+++ src/components/Layout.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import { useAPI } from "@/composables/useAPI";
+import { useMain } from "@/stores/main";
+import { type Categories } from "@/types";
+
+const { setCategories } = useMain();
+
+const getCategories = async () => {
+  const data = await useAPI("/categories");
+
+  if (data?.items) {
+    let newData = data.items as Categories[];
+
+    newData = newData.map((item) => {
+      const { id, name, productCount, imageUrl } = item;
+      return {
+        id,
+        name,
+        productCount,
+        imageUrl,
+        link: `/category/${id}`,
+      };
+    });
+    setCategories(newData);
+  }
+};
+
+getCategories();
+</script>
+
+<template>
+  <Header />
+  <main class="flex-grow max-w-screen-2xl w-full mx-auto px-6 lg:px-8 3xl:px-0">
+    <slot />
+  </main>
+  <Footer />
+</template>
