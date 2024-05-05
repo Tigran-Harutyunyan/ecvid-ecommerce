66ca39c2b75d6c63cd62825fc3a707435028fb8b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:24:09 +0400

    created categoriesList component

---
+++ src/components/CategoriesList.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { SfCounter, SfListItem } from "@storefront-ui/vue";
+
+import { RouterLink } from "vue-router";
+import { useMain } from "@/stores/main";
+import { storeToRefs } from "pinia";
+const { categories } = storeToRefs(useMain());
+</script>
+
+<template>
+  <ul>
+    <li v-for="category in categories" :key="category.id">
+      <RouterLink :to="category.link">
+        <SfListItem
+          size="sm"
+          tag="div"
+          class="first-of-type:mt-2 rounded-md active:bg-primary-100"
+        >
+          <span class="flex items-center my-1">
+            <img
+              :src="category.imageUrl"
+              alt="category image"
+              class="w-10 h-10 mr-3"
+            />
+            {{ category.name }}
+
+            <SfCounter class="ml-2 typography-text-sm font-normal">
+              {{ category.productCount }}
+            </SfCounter>
+          </span>
+        </SfListItem>
+      </RouterLink>
+    </li>
+  </ul>
+</template>
