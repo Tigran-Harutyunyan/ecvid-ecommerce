ace5f4b37313d8b2b221a999d4524b85298bbc4d
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 23:13:45 +0400

    created addToCartButton with functionality

---
+++ src/components/AddToCartButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { type Product } from "../types";
+import { SfButton, SfIconShoppingCart } from "@storefront-ui/vue";
+import { useCart } from "@/stores/cart";
+const { addToCart } = useCart();
+
+interface Props {
+  product: Product;
+  count: number;
+  size?: "sm" | "lg";
+  disabled?: boolean;
+}
+
+const { count, product, disabled, size = "sm" } = defineProps<Props>();
+</script>
+
+<template>
+  <SfButton
+    :size="size"
+    @click="addToCart(product, count)"
+    :disabled="disabled"
+  >
+    <template #prefix>
+      <SfIconShoppingCart size="sm" />
+    </template>
+    Add to cart
+  </SfButton>
+</template>
