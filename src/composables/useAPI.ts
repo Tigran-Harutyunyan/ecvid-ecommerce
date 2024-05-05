cd377ec621e417e445cee0a718ade0410b551b8b
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-05 13:56:14 +0400

    created composable to be used in test project to fetch remote data

---
+++ src/composables/useAPI.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+
+export const useAPI = async (url: string, params?: any) => {
+
+    const ECWID_API_URL = import.meta.env.VITE_ECWID_API_URL;
+    const STORE_ID = import.meta.env.VITE_ECWID_STORE_ID;
+    const ECWID_API_KEY = import.meta.env.VITE_ECWID_API_KEY;
+    const API_ENDPOINT = `${ECWID_API_URL}${STORE_ID}`;
+
+    const headers = {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + ECWID_API_KEY!,
+    };
+
+    try {
+        const { data } = await axios.get(API_ENDPOINT + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+}
