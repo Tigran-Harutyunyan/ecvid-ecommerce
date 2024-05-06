9f76e01e56a6964957bfbe4f3a2d3c5e659d6c8c
Tigran <harutyunyan.tigran1975@gmail.com>
2024-05-06 13:38:13 +0400

    changed the dropdown button color

--- src/components/CategoriesDropdown.vue
+++ src/components/CategoriesDropdown.vue
@@ -21,7 +21,7 @@ console.log(route.params);
   <Menu as="div" class="relative inline-block text-left">
     <div>
       <MenuButton
-        class="inline-flex w-full justify-center rounded-md bg-black/20 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
+        class="inline-flex w-full justify-center rounded-md bg-primary-700 px-4 py-2 text-sm font-medium text-white hover:bg-black/30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white/75"
       >
         Categories
 
